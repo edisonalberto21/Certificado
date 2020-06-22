bf53fca2cde064ae6a719f87862929b2b6e472dd
Edison Restrepo <Edison Restrepo>
2020-06-22 12:56:42 -0500

    Cambio desde login

--- src/webparts/certificados/components/Certificados.tsx
+++ src/webparts/certificados/components/Certificados.tsx
@@ -6,6 +6,7 @@ export default class Certificados extends React.Component<ICertificadosProps, {}
   public render(): React.ReactElement<ICertificadosProps> {
     return (
      <>
+     <p>Login</p>
      </>
     );
   }
